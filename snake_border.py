a1432848b3edbb3b2af76b8d07cd79b980433dcc
luigigubello <luigi.gubello@gmail.com>
2017-04-10 12:15:42 +0200

    Snake game in Python

--- snake_border.py
+++ snake_border.py
@@ -32,7 +32,7 @@ k = 0
 
 # Create snake vector
 snake = [[10,20,'#'],[10,21,'#'],[10,22,'#'],[10,23,'#'],[10,24,'#'],[10,25,'#']]                                                     
-win.addstr(snake[0][0], snake[0][1], '######')
+win.addstr(snake[0][0], snake[0][1], '######') 
 
  
 # Pick random food coordinate, avoiding the snake
